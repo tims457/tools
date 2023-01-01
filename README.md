1531af8756a0f75e82d206d790017aa27a7064df
Tim <timsullivan413@gmail.com>
2023-01-01 15:26:27 -0700

    add whisper-yt initial version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tools
+
+
+- `whisper-yt.sh` - Download audio from YouTube and transcribe to .txt file with [Whisper.cpp](https://github.com/ggerganov/whisper.cpp)
+  - requirments: 
+    - [youtube-dl](https://github.com/ytdl-org/youtube-dl#format-selection-examples)
+    - [ffmpeg](https://github.com/FFmpeg/FFmpeg)
---
+++ whisper-yt.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+print_help() {
+    echo "Usage: ./whisperyt.sh <video_url> <output_name>"
+    echo "This will download a Youtube video at <video_url> convert to .wav and transcribe the txt with whispercpp saving the file as <output_name>.txt"
+    echo "Requirements: youtube-dl, ffmpeg. Adjust the path to whisper.cpp repo and desired model as necessary below"
+}
+
+if [[ $# != 2 ]]; then
+    print_help
+    exit 1
+fi
+
+if [[ "$1" == "help" ]]; then
+    print_help
+    exit 0
+fi
+
+echo "Downloading from Youtube"
+youtube-dl -f m4a -o audio_temp.m4a $1
+echo "Converting to wav"
+ffmpeg -i audio_temp.m4a -ar 16000 -ac 1 -c:a pcm_s16le audio_temp.wav
+echo "Transcribing with Whispernet"
+# adjust paths as necessary
+~/github/whisper.cpp/main -f audio_temp.wav -m ~/github/whisper.cpp/models/ggml-small.en.bin -otxt -pp -nt -t 3 #> "$2.txt"
+mv audio_temp.wav.txt "$2.txt"
+echo "Done"
+
+
+
+echo "Cleaning up"
+rm audio_temp.*
